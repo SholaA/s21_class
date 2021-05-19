ac56f432d2ac67ad135a1a469203d0b8ceba2704
Omoshola Aleru <43586098+SholaA@users.noreply.github.com>
2021-05-19 13:18:25 -0700

    Update s21_class.py

--- s21_class.py
+++ s21_class.py
@@ -1,4 +1,8 @@
+import numpy as np
 import pandas as pd 
+import matplotlib as plt
+from scipy.stats.distributions import chi2
+
 def remove_list_item(*, the_list, the_item):
   new_list = [item for item in the_list if item != the_item]
   return new_list
@@ -11,3 +15,20 @@ def plot_x_by_class_y(*, table, x_column, y_column):
 
   pd.crosstab(table[x_column], table[y_column]).plot(kind='bar', figsize=(15,8), grid=True, logy=True)
   return None
+
+#create a gene table for proteins analyzed for positive selection. omega = overall, chi squared = Model 7 & 8 
+def paml_table_one_gene(*, nm, leng, species, sp_num,PAML, omega, title, M7, M8):
+  assert isinstance(nm, str), f'Expected a string value. Got {type(nm)} instead.' 
+  assert isinstance(leng, int), f'Expected an integer value. Got {type(leng)} instead.'
+  LR = 2*(abs(M7 - M8))
+  fin = chi2.sf(LR,5)
+  df = {'Name': nm, 
+        'Protein Length':188, 
+        'Species':species, 
+        'Max Species':sp_num ,
+        'Evolving Amino Acids':[PAML], 
+        '\u03C9 > 1':'omega', 
+        '\u03C7\u00B2 p-value': f'{fin:.5E}'} 
+  pd_df = pd.DataFrame(data= df, index = np.arange(1))
+  pd_df.style
+  return pd_df.style.set_caption(title)
